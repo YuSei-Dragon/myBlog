12a3d2206349a3ccf18a055d3a7db10f460e2720
YuSei-Dragon <3446378115@qq.com>
2026-08-13 17:16:39 +0800

    环境配置

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
+
+<style scoped>
+</style>
---
+++ vite-project/src/views/Home.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {  Search } from '@element-plus/icons-vue'
+const input = ref('')
+</script>
+
+<template>
+  <div class="home">
+    <div class="home-content">
+        <el-input
+          v-model="input"
+          style="width: 240px"
+          placeholder="Please input"
+          clearable
+          :prefix-icon="Search"
+        />
+    </div>
+  </div>
+</template>
+
+<style  lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+  background: url("../assets/fishing.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  /* opacity: 0.5; */
+  .home-content {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+}
+
+</style>
